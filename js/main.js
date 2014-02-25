b99d0088b5d7d39deac3379276b79950b50190a9
Sergey <pettson@list.ru>
2014-02-26 01:06:16 +0400

    Подключены слайдера для цитат и галереи.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
      $('.b-gallery__slider').bxSlider({
         maxSlides: 5,
-        slideWidth: 186
+        slideWidth: 186,
+        adaptiveHeight: true,
+        pager: false,
      });
 
      $('.b-quotes__slider').bxSlider({
