c38b6e2d4822972dba518ae60aa91ddde8fdd6dc
Sergey <pettson@list.ru>
2014-02-26 00:42:34 +0400

    Испорченные изображения

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,16 +12,50 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+            dynamic: {                         // Another target
+              files: [{
+                expand: true,                  // Enable dynamic expansion
+                cwd: 'img/',                   // Src matches are relative to this path
+                src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                dest: 'img/'                  // Destination path prefix
+              }]
+            }
+        },
         watch: {
             stylus: {
                 files: ['css/styl/*', 'css/styl/blocks/*'],
-                tasks: 'stylus'
+                tasks: 'stylus',
+                options: {
+                    livereload: true
+                }
+            },
+            html : {
+                files: ['*.html'],
+                options: {
+                    livereload: true
+                }
+            },
+            js : {
+                files: ['js/*', 'js/vendor/*'],
+                options: {
+                    livereload: true
+                }
+            },
+            imagemin : {
+                files: ['img/*'],
+                tasks: 'imagemin',
+                options: {
+                    livereload: true
+                }
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['stylus', 'watch']);
+
+    grunt.registerTask('default', ['stylus', 'watch', 'imagemin']);
 
 };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-     
+
+     $('.b-gallery__slider').bxSlider({
+        maxSlides: 5,
+        slideWidth: 186
+     });
+
+     $('.b-quotes__slider').bxSlider({
+        maxSlides: 1,
+        slideWidth: 630,
+        adaptiveHeight: true,
+        pager: false,
+        
+     });
 });
