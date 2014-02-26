08806d4f26f1e55decb023077e475f1137f43f2c
Sergey <pettson@list.ru>
2014-02-26 14:03:18 +0400

    Много фиксов.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         adaptiveHeight: true,
         slideMargin: 5,
         pager: false,
+        infiniteLoop: false,
      });
 
      $('.b-quotes__slider').bxSlider({
@@ -13,6 +14,6 @@ $(document).ready(function() {
         slideWidth: 630,
         adaptiveHeight: true,
         pager: false,
-        
+        infiniteLoop: false,        
      });
 });
